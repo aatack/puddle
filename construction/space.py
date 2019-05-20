7169dff7b23375b03fa675a48a16dc504a278859
aatack <alexanderjtatack@gmail.com>
2019-05-20 22:57:16 +0100

    Add placeholder functions and support for batch sizes

--- construction/space.py
+++ construction/space.py
@@ -2,31 +2,55 @@ import tensorflow as tf
 
 
 class Space:
-    def sampler(self):
+    def __init__(self, shape):
+        """Create a new space."""
+        self.shape = shape
+
+    def sampler(self, batch_size=None):
         """Create a tensorflow node that samples from the space."""
         raise NotImplementedError()
 
+    def placeholder(self, batch_size=None):
+        """
+        Create a tensorflow node that allows values to be fed in.
 
-class Scalar:
+        To exclude batches, put set the batch size to None.  For a variable batch
+        size, set it to 0.
+        """
+        shape = (
+            self.shape
+            if batch_size is None
+            else (None,) + tuple(self.shape)
+            if batch_size <= 0
+            else (batch_size,) + tuple(self.shape)
+        )
+        return tf.placeholder(tf.float32, shape=shape)
+
+
+class Scalar(Space):
     def __init__(self, lower=0.0, upper=1.0):
         """Create a new scalar with upper and lower bounds."""
+        super().__init__(tuple())
         self.lower = lower
         self.upper = upper
 
-    def sampler(self):
+    def sampler(self, batch_size=None):
         """Sample uniformly from the bounded scalar."""
-        return tf.random.uniform((), minval=self.lower, maxval=self.upper)
+        shape = () if batch_size is None else (batch_size,)
+        return tf.random.uniform(shape, minval=self.lower, maxval=self.upper)
 
 
-class Vector:
+class Vector(Space):
     def __init__(self, dimensions, lower=0.0, upper=1.0):
         """Create a new vector with upper and lower bounds."""
+        super().__init__((dimensions,))
         self.dimensions = dimensions
         self.lower = lower
         self.upper = upper
 
-    def sampler(self):
+    def sampler(self, batch_size=None):
         """Sample uniformly from the bounded hypercube."""
-        return tf.random.uniform(
-            (self.dimensions,), minval=self.lower, maxval=self.upper
+        shape = (
+            (self.dimensions,) if batch_size is None else (batch_size, self.dimensions)
         )
+        return tf.random.uniform(shape, minval=self.lower, maxval=self.upper)
