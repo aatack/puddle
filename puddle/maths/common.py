375589d301fc101abfa603fea23e4d148c67735a
aatack <alexanderjtatack@gmail.com>
2019-05-21 02:02:53 +0100

    Implement a number of common mathematical functions

---
+++ puddle/maths/common.py
@@ -0,0 +1,14 @@
+from puddle.maths.wrapper import wrap_tf_function, ShapeFunctions
+import tensorflow as tf
+
+
+copy = ShapeFunctions.copy_first_shape
+
+add = wrap_tf_function(tf.add, copy)
+subtract = wrap_tf_function(tf.subtract, copy)
+multiply = wrap_tf_function(tf.multiply, copy)
+divide = wrap_tf_function(tf.divide, copy)
+
+square = wrap_tf_function(tf.square, copy)
+sqrt = wrap_tf_function(tf.sqrt, copy)
+exp = wrap_tf_function(tf.exp, copy)
--- puddle/puddle.py
+++ puddle/puddle.py
@@ -3,6 +3,7 @@ import puddle.construction.variable as _variable
 import puddle.construction.builder as _builder
 import puddle.maths.wrapper as _wrapper
 import puddle.maths.derivatives as _derivatives
+import puddle.maths.common as _common
 
 
 space = _space.Space
@@ -17,3 +18,11 @@ buildpars = _builder.BuildParameters
 derivative = _derivatives.Derivative
 wrap_tf_function = _wrapper.wrap_tf_function
 shape_functions = _wrapper.ShapeFunctions
+
+add = _common.add
+subtract = _common.subtract
+multiply = _common.multiply
+divide = _common.divide
+square = _common.square
+sqrt = _common.sqrt
+exp = _common.exp
