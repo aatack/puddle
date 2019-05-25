b83aa5764263bc0d52166e6c30943e9c01cb4b45
aatack <alexanderjtatack@gmail.com>
2019-05-25 23:00:45 +0100

    Add persistent queries to training logs

--- puddle/api/trainer.py
+++ puddle/api/trainer.py
@@ -19,6 +19,7 @@ class Trainer:
         self.sampler_list = samplers if samplers is not None else self.default_samplers
         self.optimiser = optimiser if optimiser is not None else self.default_optimiser
         self.batch_size = batch_size or 32
+
         self.pre_batch_callbacks = (
             pre_batch_callbacks
             if pre_batch_callbacks is not None
@@ -29,7 +30,11 @@ class Trainer:
             if post_batch_callbacks is not None
             else self.default_post_batch_callbacks
         )
+
         self.batch_number = 0
+        self.epoch = None
+
+        self.queries = {}
 
         self.error = None
         self.optimise_op = None
@@ -86,19 +91,25 @@ class Trainer:
                 tf.variables_initializer(self.optimiser.variables())
             )
 
-    def train(self, iterations=1):
+    def train(self, iterations):
         """Train the system for the specified number of iterations."""
         self.initialise_training()
+        epochs = []
+
         for _ in range(iterations):
-            self.perform_training_iteration()
+            queries = self.perform_training_iteration()
+            epochs.append(queries)
+
+        return epochs
 
     def perform_training_iteration(self):
         """Train the system on one batch, including triggering all events."""
         sample = self.sampler.get_joined_sample(self.batch_size)
         self._trigger_pre_batch_events(sample)
-        self._train_on_batch(sample)
-        self._trigger_post_batch_events(sample)
+        queries = self._train_on_batch(sample)
+        self._trigger_post_batch_events(queries)
         self.batch_number += 1
+        return queries
 
     def _trigger_pre_batch_events(self, sample):
         """Call all callbacks after getting a sample but before training on it."""
@@ -106,14 +117,55 @@ class Trainer:
             for callback in self.pre_batch_callbacks:
                 callback(self, sample)
 
-    def _trigger_post_batch_events(self, sample):
-        """Call all callbacks after training on the given sample."""
+    def _trigger_post_batch_events(self, queries):
+        """Call all callbacks on the results of the given queries."""
         if len(self.post_batch_callbacks) > 0:
             for callback in self.post_batch_callbacks:
-                callback(self, sample)
+                callback(self, queries)
 
     def _train_on_batch(self, sample):
         """Optimise network parameters on the given sample."""
-        self.system.session.run(
-            self.optimise_op, feed_dict=self.system.graph.get_inputs(sample)
+        feed_dict = self.system.graph.get_inputs(sample)
+        feed_dict[self._get_epoch_node()] = self.batch_number
+
+        queries, _ = self.system.session.run(
+            (self.queries, self.optimise_op), feed_dict
         )
+        return queries
+
+    def add_query(self, *variables):
+        """
+        Add a query for a variable whose value will be returned during training.
+
+        To get the value of a variable, simply pass in the variable.  To get
+        the mean error for each sample in the batch, pass in "mean_error", or
+        to get overall error simply type "error".  "epoch" gives the current
+        training epoch.
+        """
+        self.initialise_training()
+        for variable in variables:
+            if variable in self.queries:
+                continue
+
+            if isinstance(variable, str):
+                options = self._get_string_query_options()
+                if variable in options:
+                    self.queries[variable] = options[variable]()
+                else:
+                    raise ValueError("variable '{}' unknown".format(variable))
+            else:
+                self.queries[variable] = self.system.graph.get_output(variable)
+
+    def _get_string_query_options(self):
+        """Get a list of options which can be used to add non-variable queries."""
+        return {
+            "mean_error": lambda: self.system.graph.get_mean_losses(),
+            "error": lambda: self.system.graph.get_batch_mean_loss(),
+            "epoch": lambda: self._get_epoch_node(),
+        }
+
+    def _get_epoch_node(self):
+        """Get a node which can be used to represent the current epoch."""
+        if self.epoch is None:
+            self.epoch = tf.placeholder(tf.int32, shape=())
+        return self.epoch
